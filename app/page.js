351b9a68a9d85971dd078ef8432d3b672eac0c9b
Pet002 <pet.14336@gmail.com>
2023-10-25 17:24:33 +0700

    add: item product

--- app/page.js
+++ app/page.js
@@ -5,9 +5,13 @@ export default function Home() {
     <main className='bg-white w-full min-h-screen flex justify-center' >
       <div className='text-black w-full grid grid-cols-2 gap-8 justify-center md:w-1/2 my-10 px-5'>
         
-        <a href="https://shope.ee/9UaHAMt1UM" target='_blank' className="border rounded-xl flex flex-col gap-4 item-center w-full h-[15rem]">
+        <a href="https://shope.ee/10bluQns7U" target='_blank' className="border rounded-xl flex flex-col gap-4 item-center w-full h-[15rem]">
           <img src="./asset/product1.jpg" className='w-[100%] rounded-t-xl h-[75%] object-cover'/>
           <p className='text-center'>เทียนหอม</p>
+        </a>
+        <a href="https://shope.ee/1qAstgIMvi" target='_blank' className="border rounded-xl flex flex-col gap-4 item-center w-full h-[15rem]">
+          <img src="./asset/case_iphone_black.jpg" className='w-[100%] rounded-t-xl h-[75%] object-cover'/>
+          <p className='text-center'>เคส แบรนด์ MUSE</p>
         </a>
       </div>
     </main>
